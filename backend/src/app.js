91efa3ce2113ca60f165f6f9667b7923b5d8ef31
LucianoEsplai <lfrancoepifani@gmail.com>
2024-07-16 00:40:26 +0200

    creating node backend to deploy database

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,17 @@
 import express from 'express';
 import { createPool } from 'mysql2/promise';
-import cors from 'cors'
-import { PORT } from './config.JS';
+import cors from 'cors';
+import { PORT, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } from './config.js';
 
 const app = express();
-
-app.use(cors())
+app.use(cors());
 
 const pool = createPool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME
+    user: DB_USER,
+    password: DB_PASSWORD,
+    host: DB_HOST,
+    port: DB_PORT,
+    database: DB_NAME
 });
 
 app.get('/emails', async (req, res) => {
@@ -27,7 +26,6 @@ app.get('/emails', async (req, res) => {
     }
 });
 
-// Iniciar el servidor
 app.listen(PORT, () => {
-    console.log('Server running on port', PORT);
+    console.log(`Server running on port ${PORT}`);
 });
--- backend/src/config.js
+++ backend/src/config.js
@@ -1,7 +1,6 @@
-// config.js
 export const PORT = process.env.PORT || 3000;
-export const DB_HOST = process.env.DB_HOST || 'roundhouse.proxy.rlwy.net';
+export const DB_HOST = process.env.DB_HOST || '127.0.0.1';
 export const DB_USER = process.env.DB_USER || 'root';
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'ENSKFmAdonTTSeoTJOMzPeJJGlSHRRZq';
-export const DB_NAME = process.env.DB_NAME || 'railway';
-export const DB_PORT = process.env.DB_PORT || 45584;
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'admin';
+export const DB_NAME = process.env.DB_NAME || 'dashboard';
+export const DB_PORT = process.env.DB_PORT || 3306;
